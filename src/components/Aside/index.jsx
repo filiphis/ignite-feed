e489e1636adf1326234bfde5ad0ea8c5c72f44b3
filiphis <l.filiphis@hotmail.com>
2022-07-15 14:04:15 -0300

    Add phosphoricons module

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,27 +1,31 @@
 import styles from './style.module.css';
-import buttonIcon from '../../assets/images/aside-button-icon.svg';
+import { PencilLine } from 'phosphor-react';
 import myAvatarImage from '../../assets/images/my-avatar.png';
 import asideBgImage from '../../assets/images/flowers-bg.png';
 
-export function Aside(){
-  return(
+export function Aside() {
+  return (
     <aside className={styles.aside}>
       <div className={styles.aside__wrapperBgImage}>
         <img className={styles.bgImage} src={asideBgImage} alt="" />
       </div>
 
       <div className={styles.aside__content}>
-        <img className={styles.myAvatarImage} src={myAvatarImage} alt="Foto de perfil" />
+        <img
+          className={styles.myAvatarImage}
+          src={myAvatarImage}
+          alt="Foto de perfil"
+        />
         <p className={styles.userName}>Luiz Silveira</p>
         <p className={styles.userOccupation}>Dev Front-end</p>
       </div>
 
       <div className={styles.aside__buttonWrapper}>
         <a className={styles.aside__button} href="#">
-          <img src={buttonIcon} alt="button icon" />
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </div>
     </aside>
-  )
+  );
 }
