d3802c778808706c1eaf3dd3076e35e7e7cb7044
Luiz Felipe da Silveira <l.filiphis@hotmail.com>
2022-07-17 19:19:57 -0300

    End Comment component styles

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,26 +1,30 @@
 import styles from './style.module.css';
 import avatarImage from '../../assets/images/my-avatar.png'
-import { Trash } from 'phosphor-react';
+import { Trash, ThumbsUp } from 'phosphor-react';
 
 export function Comment() {
   return (
     <div className={styles.comment}>
+
+
       <div className={styles.comment__imageBox}>
-        <img src={avatarImage} alt="User avatar" />
+        <img className={styles.comment__avatar} src={avatarImage} alt="User avatar" />
       </div>
 
-      <div>
-        <header className={styles.comment__header}>
-          <div className={style.comment__userInfo}>
-            <span className={styles.comment__userName}>Luiz Silveira</span>
-            <time className={styles.time} title='17 de julho as 03:15h' dateTime='2022-07-17 03:15:00'>Comentou há 1h</time>
-          </div>
-          <Trash size={20} />
-        </header>
-
+      <div className={styles.comment__box}>
         <div className={styles.comment__content}>
+          <header className={styles.comment__header}>
+            <div className={styles.comment__userInfo}>
+              <span className={styles.comment__userName}>Luiz Silveira</span>
+              <time className={styles.time} title='17 de julho as 03:15h' dateTime='2022-07-17 03:15:00'>Comentou há 1h</time>
+            </div>
+            <Trash size={24} className={styles.comment__trashIcon} />
+          </header>
+
           <p className={styles.comment__message}>Muito bom Devon, parabéns!! 👏👏</p>
         </div>
+        
+
 
         <footer className={styles.comment__footer}>
           <button className={styles.comment__likeButton}>
@@ -29,7 +33,13 @@ export function Comment() {
             <span>06</span>
           </button>
         </footer>
+
       </div>
+
+
+
+
+
     </div>
   )
 }
