5bb0eb21c34f536acc68cd45f46cc3c204e4ecde
Luiz Felipe da Silveira <l.filiphis@hotmail.com>
2022-07-17 17:03:35 -0300

    Add Post component styles

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -6,10 +6,10 @@ export function Post(props) {
     <div className={styles.post}>
       <header className={styles.header}>
         <div className={styles.user}>
-          <img src={userAvatar} alt="" />
+          <img className={styles.userAvatar} src={userAvatar} alt="" />
           <div className={styles.userInfo}>
             <span className={styles.userName}>Luiz Silveira</span>
-            <span className={styles.userOccupation}>Luiz Silveira</span>
+            <span className={styles.userOccupation}>Dev Front-End</span>
           </div>
         </div>
         <time className={styles.time} title='17 de julho as 03:15h' dateTime='2022-07-17 03:15:00'>Públicado há 1h</time>
@@ -18,8 +18,20 @@ export function Post(props) {
         <p>Fala galeraa 👋</p>
         <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
         <p>👉 <a href="#">jane.design/doctorcare</a></p>
-        <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+        <p>
+          <a href="#">#novoprojeto</a> {' '}
+          <a href="#">#nlw</a>{' '}
+          <a href="#">#rocketseat</a>
+
+          </p>
       </div>
+      <form className={styles.form}>
+        <legend className={styles.form__title}>Deixe seu feedback</legend>
+        <textarea className={styles.form__text} placeholder='Escreva um comentário...' />
+        <div className={styles.form__buttonWrapper}>
+          <button className={styles.form__button} type="submit">Publicar</button>
+        </div>
+      </form>
     </div>
   )
 }
